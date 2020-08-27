4642ec8c9e8ece8f6e1bee4d2db79299b00dc7b8
melissa-mg <melissali@Melissas-MacBook-Pro.local>
2020-08-27 11:18:10 -0700

    add heap building block models

---
+++ models/heap_poc/dimension/heap_dimension_firm.sql
@@ -0,0 +1,15 @@
+{{ config(materialized='table', alias='dimension_firm') }}
+
+SELECT
+    account_id,
+    MAX(firm_id::int) AS legacy_firm_id,
+    MAX(contact_firm_name_text__C) AS firm_name,
+    COUNT(distinct contact_id) AS associated_contacts,
+    MIN(joindate) AS heap_date_created,
+    MAX(last_modified) AS heap_last_modified,
+    MAX(contact_lastactivitydate) AS salesforce_last_activity_date,
+    MAX(CASE WHEN contact_subscription_status_text__C LIKE 'Active %' THEN 1 ELSE 0 END) AS has_active_sub,
+    MAX(CASE WHEN contact_subscription_status_text__C = 'Active (Paid)' THEN 1 ELSE 0 END) AS has_paid_sub
+FROM {{ source('heap', 'users') }}
+WHERE account_id IS NOT NULL
+GROUP BY 1
---
+++ models/heap_poc/fact/heap_fact_data_downloads.sql
@@ -0,0 +1,65 @@
+{{
+    config(
+        materialized='incremental', 
+        alias='fact_data_downloads'
+    ) 
+}}
+
+SELECT
+    user_id,
+    event_id,
+    session_id,
+    time AS event_time,
+    session_time AS session_start_time,
+    platform,
+    device_type,
+    browser,
+    country,
+    region,
+    city,
+    ip,
+    referrer,
+    landing_page,
+    landing_page_query,
+    landing_page_hash,
+    path,
+    query,
+    hash,
+    utm_source,
+    utm_campaign,
+    utm_medium,
+    utm_term,
+    utm_content,
+    -- same definition as this Heap custom property https://heapanalytics.com/app/definitions?view=properties&type=defined_property&id=Profile-Type-301892
+    CASE
+        WHEN path ILIKE '/deal/%' THEN 'Deal Profile'
+        WHEN path ILIKE '/serviceprovider/%' THEN 'Service Provider Profile'
+        WHEN path ILIKE '/asset/%' THEN 'Asset Profile'
+        WHEN path ILIKE '/investmentconsultant/%' THEN 'Investment Consultant'
+        WHEN path ILIKE '/fundmanager/%' THEN 'Fund Manager Profile'
+        WHEN path ILIKE '/funds/%' THEN 'Fund Profile'
+        WHEN path ILIKE '/investor/%' THEN 'Investor Profile'
+    END AS profile_type,
+    -- same definition as this Heap custom property https://heapanalytics.com/app/definitions?view=properties&type=defined_property&id=App-Section-Category-301869
+    CASE
+        WHEN profile_type IS NOT NULL THEN 'Profile'
+        WHEN path ILIKE '/dashboard/watchlist%' OR path ILIKE '/discover%targetlists%' THEN 'Target List'
+        WHEN path = '/dashboard/alerts' THEN 'Alerts'
+        WHEN path = '/dashboard/savedSearch' THEN 'Saved Searches'
+        WHEN path ILIKE '%mybenchmarks%' OR path ILIKE '/dashboard/benchmark/%/custom%' OR path ILIKE '/discoverAnalysis/benchmarks/%/custom' THEN 'My Benchmarks'
+        WHEN path = '/analysis/horizonIRRs' OR path ILIKE '%benchmarks%' OR path ILIKE '/discoverAnalysis/benchmarks/%/market%' THEN 'Market Benchmarks'
+        WHEN path ILIKE '/discover%' OR path = '/search' THEN 'Search'
+        WHEN path ILIKE '/analysis%' THEN 'Charts'
+        WHEN path IN ('/dashboard', '/dashboard/') THEN 'Dashboard Home'
+        WHEN path ILIKE '/portfolio%' THEN 'My Portfolio'
+        WHEN path = '/datasupport' THEN 'Support'
+    END AS app_section_category
+FROM {{ source('heap', 'mammoth_pro_key_actions_data_table_download_confirmed') }}
+WHERE app_section_category IS NOT NULL
+
+{% if is_incremental() %}
+
+  -- this filter will only be applied on an incremental run
+  and event_time > (select max(event_time) from {{ this }})
+
+{% endif %}
