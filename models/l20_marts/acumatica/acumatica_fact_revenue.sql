5bb811a6481e315a0c6b0e8eab6a33f38e8e5a41
Giles Carlin <Giles.Carlin@preqin.com>
2020-10-29 12:13:15 +0000

    removed mention of firm and parentfirm from fact

--- models/l20_marts/acumatica/acumatica_fact_revenue.sql
+++ models/l20_marts/acumatica/acumatica_fact_revenue.sql
@@ -13,9 +13,7 @@ WITH revenue AS (
        coalesce(r.dimension_region_key,'-1')    																																	 as dimension_region_key,
        coalesce(p.dimension_product_key,'-1')    																																	 as dimension_product_key,
        coalesce(dac.dimension_account_classification_key,'-1')    																													 as dimension_account_classification_key,
-       --Commented out 28-10-2020 as not needed for MVP currently.
-       --coalesce(f.dimension_firm_key,'-1')    																																		 as dimension_firm_key,
-       --coalesce(pf.dimension_parent_firm_key,'-1')    																																 as dimension_parent_firm_key  
+
     FROM 
         {{ ref('stg_acumatica_book_of_business') }} a
     
@@ -30,12 +28,6 @@ WITH revenue AS (
 
     LEFT JOIN {{ ref('acumatica_dimension_account_classification') }} dac
         ON a.account_classification = dac.account_classification
-    
-    --LEFT JOIN {{ ref('acumatica_dimension_firm') }} f
-    --ON a.firm_id = f.firm_id
-
-    --LEFT JOIN {{ ref('acumatica_dimension_parent_firm') }} pf
-    --ON a.parent_firm_id = pf.parent_firm_id
 
     WHERE list_price_per_product = '1'
 ),
