29f27ced108546822e29e3beff76aa4dfa9ad45f
melissa-mg <melissali@Melissas-MacBook-Pro.local>
2020-12-09 11:02:57 -0800

    undo daily profile view changes

--- models/presentation_poc/presentation_heap_daily_profile_views.sql
+++ models/presentation_poc/presentation_heap_daily_profile_views.sql
@@ -13,12 +13,18 @@ WITH app_pageviews AS
         event_time,
         event_id,
         session_id,
+        app_section_category,
         profile_type, 
         profile_id,
         profile_section
-    FROM {{ ref('heap_fact_profile_page_viewed') }} AS profile_page_viewed
+    FROM {{ ref('heap_fact_app_page_viewed') }} AS app_page_viewed
     JOIN {{ ref('heap_dimension_user') }} AS users
-        ON profile_page_viewed.user_id = users.user_id
+        ON app_page_viewed.user_id = users.user_id
+   /* WHERE account_id IS NOT NULL
+        AND app_section_category IS NOT NULL
+    */
+    WHERE app_section_category IS NOT NULL
+
 )
 SELECT
     event_time::date AS date,
@@ -29,7 +35,7 @@ SELECT
     legacy_firm_id,
     contact_name,
     sales_region,
-    'Profile' as app_section_category,
+    app_section_category,
     profile_type,
     profile_id,
     profile_section,
