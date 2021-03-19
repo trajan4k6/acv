8b3cf818ebe475207c103dca805162f45863d8c7
StuartBadcock <57263350+StuartBadcock@users.noreply.github.com>
2021-03-19 12:59:00 +0000

    As a result a Mammoth's latest changes (contact dashboard): (#32)
    i) Updated model fact_usage_daily_profile_views - now just profile views
     and ii) added fact_usage_daily_app_page_views - includes all Pro page views

--- models/l20_marts/presentation/fact_usage_daily_app_page_views.sql
+++ models/l20_marts/presentation/fact_usage_daily_app_page_views.sql
@@ -17,15 +17,9 @@ SELECT
 CAST(YEAR((Fact.DATE::DATE)) || RIGHT('0' || MONTH((Fact.DATE::DATE)), 2) || RIGHT('0' || DAYOFMONTH((Fact.DATE::DATE)), 2) AS INT) AS DATE_KEY,
 CASE WHEN U.conformed_dimension_individual_key = '-1' THEN U.dimension_user_key ELSE COALESCE(U.conformed_dimension_individual_key,'-1') END DIMENSION_INDIVIDUAL_KEY,
 CASE WHEN F.conformed_dimension_firm_key = '-1' THEN F.dimension_firm_key ELSE COALESCE(F.conformed_dimension_firm_key,'-1') END DIMENSION_FIRM_KEY,
-CASE WHEN lower(profile_type) = ('fundmanager') THEN GP.dimension_firm_key ELSE NULL END dimension_firm_GP_profile_key,
-CASE WHEN lower(profile_type) = ('investor') THEN LP.dimension_firm_key ELSE NULL END dimension_firm_LP_profile_key,
-CASE WHEN lower(profile_type) = ('serviceprovider') THEN SP.dimension_firm_key ELSE NULL END dimension_firm_ServiceProvider_profile_key,
-CASE WHEN lower(profile_type) = ('consultant') THEN IC.dimension_firm_key ELSE NULL END dimension_firm_consultant_profile_key,
-profile_type,
-profile_id,
-profile_section,
-SUM(profile_view_count) AS profile_view_count
-FROM {{ ref('presentation_heap_daily_profile_views') }} Fact
+app_section_category,
+SUM(app_page_view_count) AS total_page_views
+FROM {{ ref('presentation_heap_daily_app_page_views') }} Fact
 LEFT
 JOIN {{ ref('heap_dimension_firm_integrated') }} F
     ON Fact.Account_ID = F.Account_ID
@@ -35,16 +29,9 @@ JOIN {{ ref('salesforce_dimension_account') }} A
 LEFT
 JOIN heap_identity U
     ON Fact.Identity = U.Identity
-LEFT
-JOIN {{ ref('dimension_firm') }} GP
-    ON Fact.profile_id = GP.FIRM_ID
-LEFT
-JOIN {{ ref('dimension_firm') }} LP
-    ON Fact.profile_id = LP.FIRM_ID
-LEFT
-JOIN {{ ref('dimension_firm') }} SP
-    ON Fact.profile_id = SP.FIRM_ID
-LEFT
-JOIN {{ ref('dimension_firm') }} IC
-    ON Fact.profile_id = IC.FIRM_ID
-{{ dbt_utils.group_by(n=10) }}
+{{ dbt_utils.group_by(n=4) }}
+
+
+
+
+ 
