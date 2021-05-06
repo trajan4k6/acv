fc878493f267a761bf14ef30ec0193ff978a1b83
Gerard Morgan <Gerard.Morgan@preqin.com>
2021-05-06 08:51:49 +0100

    update to regular expression

--- models/l20_marts/salesforce/bridge_opportunity_to_order.sql
+++ models/l20_marts/salesforce/bridge_opportunity_to_order.sql
@@ -1,5 +1,5 @@
 {{ config(materialized='table') }}
 
-select ORDERID, COLLATE(regexp_substr(NOTES, '\\SF\\sID:\\W+(\\w+)', 1, 1, 'ime', 1),'upper') as "OPPORTUNITYID" 
+select ORDERID, COLLATE(regexp_substr(NOTES, '\\SF\\sID\\W+(\\w+)', 1, 1, 'ime', 1),'upper') as "OPPORTUNITYID" 
 FROM {{ source('preqin', 'tblorders') }}
-WHERE regexp_substr(NOTES, '\\SF\\sID:\\W+(\\w+)', 1, 1, 'ime', 1) IS NOT NULL
+WHERE regexp_substr(NOTES, '\\SF\\sID\\W+(\\w+)', 1, 1, 'ime', 1) IS NOT NULL
