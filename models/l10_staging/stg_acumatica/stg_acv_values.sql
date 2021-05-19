37369f74bf7b907e0dabefe32f4aa2c487c84629
trajan4k6 <82454873+trajan4k6@users.noreply.github.com>
2021-05-19 12:58:40 +0100

    acv_growth_levers

---
+++ models/l10_staging/stg_acumatica/stg_acv_values.sql
@@ -0,0 +1,37 @@
+{{ config(materialized='table') }}
+
+WITH source AS (
+SELECT * FROM  {{ source('acv', 'acv_values_2020') }}
+),
+renamed AS (
+    SELECT
+			"Firm ID" FIRM_ID,
+			"Lookup ID" LOOKUP_ID,
+			"Firm Name" FIRM_NAME,
+			"Firm Tier" FIRM_TIER,
+			"Parent Firm Type" PARENT_FIRM_TYPE,
+			"Firm Category"  FIRM_CATEGORY,
+			"Inventory Description" INVENTORY_DESCRIPTION,
+			"Asset Class" ASSET_CLASS,
+			"Product Type" PRODUCT_TYPE,
+			"Month Reflected In Bank" AS_AT_DATE,
+			"Region" REGION,
+			"Country" COUNTRY,
+			"State" STATE,
+			"Currency" CURRENCY,ACV_GBP,
+			"Start Date" START_DATE,
+			"End Date" END_DATE
+
+    FROM source
+
+),
+
+final AS (
+
+    SELECT 
+    *                 
+FROM renamed
+
+)
+
+SELECT * FROM final
