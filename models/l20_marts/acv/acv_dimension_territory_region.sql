712481ee097ee1ac66fd4307554534d8d3f9fec9
Thangaraj Rajan <thangaraj.rajan@preqin.com>
2021-06-17 17:50:54 +0000

    Sales_region in acv_dim_terr_reg and Start_End_Date in acv_growth_levers added

--- models/l20_marts/acv/acv_dimension_territory_region.sql
+++ models/l20_marts/acv/acv_dimension_territory_region.sql
@@ -4,15 +4,16 @@ WITH region AS
     (
         SELECT
             {{ dbt_utils.surrogate_key(
-                [7,'region']
+                [7,'sales_region','region']
             ) }}                       as dimension_territory_region_key,
             region,
+            sales_region,
             7 as datasource_id
         FROM
              {{ ref('stg_acv_values') }} a
         WHERE 
             region IS NOT NULL
-        GROUP BY region
+        GROUP BY region,sales_region
     )
 
 SELECT
